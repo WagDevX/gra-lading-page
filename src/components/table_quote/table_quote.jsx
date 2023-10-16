ccf5ced1a7715817a50d8df9585932ef4e21bed9
WagDevX <waguinbk@gmail.com>
2023-10-15 21:20:13 -0300

    bug-fix-try

--- src/components/table_quote/table_quote.jsx
+++ src/components/table_quote/table_quote.jsx
@@ -8,10 +8,6 @@ export default function TableQuote() {
   const quoteToPrintRef = useRef(null);
   const [table, setTable] = useState([]);
 
-  function addItem() {
-
-  }
-
   return (
   <div class="grid w-full">
     <h1 className="text-primary text-3xl font-bold">Geração de orçamentos</h1>
@@ -80,14 +76,14 @@ export default function TableQuote() {
             </div>
 
     <div className='hidden'>
-    {/* <QuoteToPrint ref={quoteToPrintRef}  dados={table}/> */}
+    <QuoteToPrint ref={quoteToPrintRef}  dados={table}/>
     </div>  
-    <ReactToPrint
+    {/* <ReactToPrint
               trigger={() => <button  className=" mt-5 bg-primary hover:after:text-white  text-white border-transparent hover:border-[#172554] px-6 py-3  rounded-full outline-none relative overflow-hidden border duration-300 ease-linear
               after:absolute after:inset-x-0 after:aspect-square after:scale-0 after:opacity-70 after:origin-center after:duration-300 after:ease-linear after:rounded-full after:top-0 after:left-0 after:bg-[#172554]
                hover:after:opacity-100 hover:after:scale-[2.5]">Imprimir</button>}
               content={() => quoteToPrintRef.current}
-            />
+            /> */}
   </div>  
    
   );
