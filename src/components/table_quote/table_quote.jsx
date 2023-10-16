4fa5881b645459f1f5b0a68d97a681bcac367f92
WagDevX <waguinbk@gmail.com>
2023-10-15 21:14:12 -0300

    yarn fix

--- src/components/table_quote/table_quote.jsx
+++ src/components/table_quote/table_quote.jsx
@@ -82,12 +82,12 @@ export default function TableQuoe() {
     <div className='hidden'>
     {/* <QuoteToPrint ref={quoteToPrintRef}  dados={table}/> */}
     </div>  
-    {/* <ReactToPrint
+    <ReactToPrint
               trigger={() => <button  className=" mt-5 bg-primary hover:after:text-white  text-white border-transparent hover:border-[#172554] px-6 py-3  rounded-full outline-none relative overflow-hidden border duration-300 ease-linear
               after:absolute after:inset-x-0 after:aspect-square after:scale-0 after:opacity-70 after:origin-center after:duration-300 after:ease-linear after:rounded-full after:top-0 after:left-0 after:bg-[#172554]
                hover:after:opacity-100 hover:after:scale-[2.5]">Imprimir</button>}
               content={() => quoteToPrintRef.current}
-            /> */}
+            />
   </div>  
    
   );
